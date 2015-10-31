a59f37bc2eeae398aa49ab65c44b2211ce8c30c9
countingtoten <countingtoten@gmail.com>
2015-10-31 16:40:03 -0500

    Prep for Heroku deploy

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "TXJS 2015",
+  "description": "",
+  "repository": "https://github.com/countingtoten/txjs2015",
+  "logo": "https://node-js-sample.herokuapp.com/node.png",
+  "keywords": ["node", "express", "static"]
+}
