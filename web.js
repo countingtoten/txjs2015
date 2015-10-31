5b58f3d311707b61daaa63d8d1c86fb13c8f76e2
countingtoten <countingtoten@gmail.com>
2015-10-31 17:22:25 -0500

    Fix missing middleware

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var logfmt = require('logfmt');
 var app = express();
-app.use(express.logger());
+app.use(logfmt.requestLogger());
 app.use('/', express.static(__dirname));
 
 var port = process.env.PORT || 5000;
